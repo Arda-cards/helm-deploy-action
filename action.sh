a4f4cb4e98949f0f3407c7dd2bcbb47f7e40b87c
Denis N. Antonioli <1205618+denisa@users.noreply.github.com>
2025-01-17 16:21:02 -0800

    1: w/out --export

--- action.sh
+++ action.sh
@@ -73,7 +73,7 @@ namespace="${phase}-${module_name}"
 if [ "${dry_run:-false}" = "false" ]; then
   /usr/local/bin/kubectl create namespace "${namespace}" --dry-run=client -o yaml |
    /usr/local/bin/kubectl apply -f -
-  /usr/local/bin/kubectl get secret regcred --namespace=default --export -o yaml |
+  /usr/local/bin/kubectl get secret regcred --namespace=default -o yaml |
     /usr/local/bin/kubectl apply --namespace="${namespace}" -f -
 fi
 
